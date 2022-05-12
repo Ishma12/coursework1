9e7f1029e9a0e2b30e233890e432a376a974c298
Ishma12 <102284247+Ishma12@users.noreply.github.com>
2022-05-12 11:29:19 +0545

    Add files via upload

---
+++ getinfo.php
@@ -0,0 +1,40 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+// Select weather data for given parameters-------------------
+$sql = "SELECT *
+FROM weatherdata
+WHERE weather_city = '{$_GET['city']}'
+AND datetimes >= DATE_SUB(NOW(), INTERVAL 10 HOUR)
+ORDER BY datetimes DESC limit 1";
+$result = $mysqli -> query($sql);
+
+// If 0 record found
+if ($result->num_rows == 0) {
+$url = 'https://api.openweathermap.org/data/2.5/weather?q='.$_GET['city'].' &appid=a264d9fa5a347c3724befe77fcc87043&units=metric';
+// Get data from openweathermap and store in JSON object
+$data = file_get_contents($url);
+$json = json_decode($data, true);
+// Fetch required fields
+$weather = $json['weather'][0]['description'];
+$temperature = $json['main']['temp'];
+$wind = $json['wind']['speed'];
+$datetimes = date("Y-m-d H:i:s"); 
+$weather_city = $json['name'];
+$pressure=$json ['main'] ['pressure'];
+$speed=$json['wind']['speed'];
+$humidity=$json['main']['humidity'];
+$icon=$json['weather'] [0] ['icon'];
+
+
+// Build INSERT SQL statement
+$sql = "INSERT INTO  weatherdata (weather,temperature ,wind ,datetimes ,speed ,humidity,weather_city,pressure,icon)
+VALUES('{$weather}','{$temperature}','{$wind}','{$datetimes}','{$speed}','{$humidity}','{$weather_city}','{$pressure}','{$icon}')";
+// Run SQL statement and report errors
+//var_dump($sql);
+if (!$mysqli -> query($sql)) {
+echo("<h4>SQL error description: " . $mysqli -> error . "</h4>");
+}
+}
+?>
---
+++ weatherdata.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2022 at 04:49 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `weatherdetails`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weatherdata`
+--
+
+CREATE TABLE `weatherdata` (
+  `id` int(10) NOT NULL,
+  `weather` varchar(25) NOT NULL,
+  `temperature` float NOT NULL,
+  `wind` float NOT NULL,
+  `datetimes` datetime NOT NULL,
+  `speed` float NOT NULL,
+  `humidity` int(20) NOT NULL,
+  `weather_city` varchar(20) NOT NULL,
+  `pressure` int(20) NOT NULL,
+  `icon` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `weatherdata`
+--
+
+INSERT INTO `weatherdata` (`id`, `weather`, `temperature`, `wind`, `datetimes`, `speed`, `humidity`, `weather_city`, `pressure`, `icon`) VALUES
+(29, 'heavy intensity rain', 19.44, 0.45, '2022-01-12 04:40:24', 0.45, 95, 'Miami', 1023, '10n');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `weatherdata`
+--
+ALTER TABLE `weatherdata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `weatherdata`
+--
+ALTER TABLE `weatherdata`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
